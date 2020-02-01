86a84a4448db273d905f5b254c227ed2b0680b52
Roy Tam <roytam@gmail.com>
2020-02-01 17:36:39 +0800

    PSM: add and enable ChaCha20-Poly1305

--- security/manager/ssl/src/nsNSSComponent.cpp
+++ security/manager/ssl/src/nsNSSComponent.cpp
@@ -1066,6 +1066,8 @@ static CipherPref CipherPrefs[] = {
  {"security.ssl3.ecdhe_rsa_aes_128_gcm_sha256", TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256}, // 128-bit AES-GCM encryption with ECDHE-RSA
  {"security.ssl3.ecdhe_rsa_aes_128_sha256", TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256}, // 128-bit AES-CBC encryption with ECDHE-RSA
  {"security.ssl3.ecdhe_rsa_aes_256_gcm_sha384", TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384}, // 256-bit AES-GCM encryption with ECDHE-RSA
+ {"security.ssl3.ecdhe_ecdsa_chacha20_poly1305_sha256", TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256},
+ {"security.ssl3.ecdhe_rsa_chacha20_poly1305_sha256", TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256 },
  {NULL, 0} /* end marker */
 };
 
